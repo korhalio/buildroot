33223138edbd494e625787697b5c6177277fe62d
Arvid E. Picciani <aep@exys.org>
2017-12-14 11:46:10 +0100

    enable static linking for libfuse
    Signed-off-by: Arvid E. Picciani <aep@exys.org>

--- package/libfuse/libfuse.mk
+++ package/libfuse/libfuse.mk
@@ -12,13 +12,14 @@ LIBFUSE_LICENSE_FILES = COPYING COPYING.LIB
 LIBFUSE_INSTALL_STAGING = YES
 LIBFUSE_DEPENDENCIES = $(if $(BR2_PACKAGE_LIBICONV),libiconv)
 LIBFUSE_CONF_OPTS = \
+	--enable-static \
 	--disable-example \
 	--enable-lib \
 	--enable-util
 
 define LIBFUSE_INSTALL_TARGET_CMDS
 	cp -dpf $(STAGING_DIR)/usr/bin/fusermount $(TARGET_DIR)/usr/bin/
-	cp -dpf $(STAGING_DIR)/usr/lib/libfuse.so* $(TARGET_DIR)/usr/lib/
+	cp -dpf $(STAGING_DIR)/usr/lib/libfuse.a* $(TARGET_DIR)/usr/lib/
 endef
 
 $(eval $(autotools-package))
